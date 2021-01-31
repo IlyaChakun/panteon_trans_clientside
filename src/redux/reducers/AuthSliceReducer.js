bb3ca9c14d8b440b0366a04fb5e884d88140b56a
Yuriy_Lukashevich <yurikluk@gmail.com>
2021-01-31 23:34:08 +0300

    add fun updateProfile

--- src/redux/reducers/AuthSliceReducer.js
+++ src/redux/reducers/AuthSliceReducer.js
@@ -1,108 +1,101 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit'
 import {
-    getCurrentCompanyRequest,
-    getCurrentUserRequest,
-    getProductsByShopIdRequest,
-    getProductsRequest
-} from "../../components/util/utilsAPI";
-import {notification} from "antd";
+  getCurrentCompanyRequest,
+  getCurrentUserRequest, updateUserProfileRequest
+} from '../../components/util/utilsAPI'
 
 const initialState = {
-    isLoading: false,
-    errors: '',
-    currentUser: null,
-    currentCompany: null,
-    isAuthenticated: false,
+  isLoading: false,
+  errors: '',
+  currentUser: null,
+  currentCompany: null,
+  isAuthenticated: false
 }
 
-
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setIsLoading: (state, payload) => {
-            state.isLoading = payload
-        },
-        setErrors: (state, payload) => {
-            state.errors = payload
-        },
-        setCurrentUser: (state, payload) => {
-            state.currentUser = payload
-        },
-        setCurrentCompany: (state, payload) => {
-            state.currentCompany = payload
-        },
-        setIsAuthenticated: (state, payload) => {
-            state.isAuthenticated = payload
-        }
+  name: 'auth',
+  initialState,
+  reducers: {
+    setIsLoading: (state, payload) => {
+      state.isLoading = payload
     },
+    setErrors: (state, payload) => {
+      state.errors = payload
+    },
+    setCurrentUser: (state, payload) => {
+      state.currentUser = payload
+    },
+    setCurrentCompany: (state, payload) => {
+      state.currentCompany = payload
+    },
+    setIsAuthenticated: (state, payload) => {
+      state.isAuthenticated = payload
+    }
+  }
 })
 export const {
-    setIsLoading,
-    setErrors,
-    setCurrentUser,
-    setCurrentCompany,
-    setIsAuthenticated
+  setIsLoading,
+  setErrors,
+  setCurrentUser,
+  setCurrentCompany,
+  setIsAuthenticated
 } = authSlice.actions
 
 export default authSlice.reducer
 
 export const authSelector = (state) => {
-    return state.authState;
+  return state.authState
 }
-
 
 export const getCurrentUser = () => {
-    return async dispatch => {
-        dispatch(setIsLoading(true))
-        try {
-            let promise = getCurrentUserRequest()
+  return async dispatch => {
+    dispatch(setIsLoading(true))
+    try {
+      const promise = getCurrentUserRequest()
 
-            if (!promise) {
-                return;
-            }
-            promise
-                .then(response => {
-                    console.log(response)
-                    dispatch(setCurrentUser(response))
-                    dispatch(setIsAuthenticated(true))
-                    dispatch(setIsLoading(false))
-                })
-        } catch (error) {
-            dispatch(setErrors(error))
-            dispatch(setIsLoading(false))
-        }
+      if (!promise) {
+        return
+      }
+      promise
+        .then(response => {
+          console.log(response)
+          dispatch(setCurrentUser(response))
+          dispatch(setIsAuthenticated(true))
+          dispatch(setIsLoading(false))
+        })
+    } catch (error) {
+      dispatch(setErrors(error))
+      dispatch(setIsLoading(false))
     }
+  }
 }
 
-
-// First, create the thunk
-const fetchUser = createAsyncThunk(
-    'users/fetchBy',
-    async ( thunkAPI) => {
-        const response = await getCurrentUser()
-        return response
-    }
-)
+export const updateUserProfile = (updateUserRequest) => {
+  return async dispatch => {
+    const profile = await updateUserProfileRequest(updateUserRequest)
+    dispatch(setIsLoading(true))
+    dispatch(setCurrentUser(profile))
+    dispatch(setIsLoading(false))
+  }
+}
 
 export const getCurrentCompany = () => {
-    return async dispatch => {
+  return async dispatch => {
+    try {
+      const promise = getCurrentCompanyRequest()
 
-        try {
-            let promise = getCurrentCompanyRequest()
-
-            if (!promise) {
-                return;
-            }
-            promise
-                .then(response => {
-                    console.log(response)
-                    dispatch(setCurrentCompany(response))
-                    dispatch(setIsLoading(false))
-                })
-        } catch (error) {
-            dispatch(setErrors(error))
-            dispatch(setIsLoading(false))
-        }
+      if (!promise) {
+        return
+      }
+      promise
+        .then(response => {
+          console.log(response)
+          dispatch(setCurrentCompany(response))
+          dispatch(setIsLoading(false))
+        })
+    } catch (error) {
+      dispatch(setErrors(error))
+      dispatch(setIsLoading(false))
     }
+  }
 }
