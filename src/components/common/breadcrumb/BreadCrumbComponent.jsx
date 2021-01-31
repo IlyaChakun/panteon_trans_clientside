df38be6e5db488182a02dd79c1b36ac26c069d72
Yuriy_Lukashevich <yurikluk@gmail.com>
2021-02-01 02:18:47 +0300

    breadcrumbs to func

--- src/components/common/breadcrumb/BreadCrumbComponent.jsx
+++ src/components/common/breadcrumb/BreadCrumbComponent.jsx
@@ -1,59 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Breadcrumb } from 'antd'
 import { Link } from 'react-router-dom'
 
-class BreadCrumbComponent extends Component {
-  render () {
-    const breadcrumbNameMap = {
-      '/login': 'Авторизация',
-      '/sign-up': 'Регистрация',
-      '/profile': 'Личный кабинет',
-      '/company/shops/:id': 'Детальный просмотр магазина',
-      '/company': 'Компания',
-      '/reviews': 'Отзывы пользователей',
-      '/company/shops': 'Магазины компании',
-      '/basket': 'Корзина',
-      '/bouquets': 'Букеты',
-      '/flowers': 'Цветы поштучно',
-      '/orders/:id': 'Детальный просмотр заказа'
-      //TODO на детальных заказах не отрабатывает
-      //TODO на детальных магазинах не отрабатывает
-    }
+function BreadCrumbComponent(props) {
+  const breadcrumbNameMap = {
+    '/login': 'Авторизация',
+    '/sign-up': 'Регистрация',
+    '/profile': 'Личный кабинет',
+    '/company/shops/:id': 'Детальный просмотр магазина',
+    '/company': 'Компания',
+    '/reviews': 'Отзывы пользователей',
+    '/company/shops': 'Магазины компании',
+    '/basket': 'Корзина',
+    '/bouquets': 'Букеты',
+    '/flowers': 'Цветы поштучно',
+    '/orders/:id': 'Детальный просмотр заказа'
+    //TODO на детальных заказах не отрабатывает
+    //TODO на детальных магазинах не отрабатывает
+  }
 
-    const { location } = this.props.properties
-    const pathSnippets =
-            location
-              .pathname.split('/')
-              .filter(i => i)// return break url one by one
-              .filter(i => { // if it `s a number then miss it if not number it`s must be in url map
-                if (!Number.isInteger(Number.parseInt(i))) {
-                  return i
-                } else {
-                  return ''
-                }
-              })
+  const { location } = props.properties
+  const pathSnippets =
+    location
+      .pathname.split('/')
+      .filter(i => i)// return break url one by one
+      .filter(i => { // if it `s a number then miss it if not number it`s must be in url map
+        if (!Number.isInteger(Number.parseInt(i))) {
+          return i
+        } else {
+          return ''
+        }
+      })
 
-    const extraBreadcrumbItems = pathSnippets.map((_, index) => {
-      const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
+  const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
 
-      console.log(url)
-
-      return (
-        <Breadcrumb.Item key={url}>
-          <Link to={url}>{breadcrumbNameMap[url]}</Link>
-        </Breadcrumb.Item>
-      )
-    })
-    const breadcrumbItems = [
-      <Breadcrumb.Item key="home">
-        <Link to="/">Главная страница</Link>
-      </Breadcrumb.Item>
-    ].concat(extraBreadcrumbItems)
+    console.log(url)
 
     return (
-      <Breadcrumb>{breadcrumbItems}</Breadcrumb>
+      <Breadcrumb.Item key={url}>
+        <Link to={url}>{breadcrumbNameMap[url]}</Link>
+      </Breadcrumb.Item>
     )
-  }
+  })
+  const breadcrumbItems = [
+    <Breadcrumb.Item key='home'>
+      <Link to='/'>Главная страница</Link>
+    </Breadcrumb.Item>
+  ].concat(extraBreadcrumbItems)
+
+  return (
+    <Breadcrumb>{breadcrumbItems}</Breadcrumb>
+  )
 }
 
 export default BreadCrumbComponent
