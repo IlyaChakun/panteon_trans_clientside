0e9100e9a5321628a464d53bf41e8b99bfe88058
Yuriy_Lukashevich <yurikluk@gmail.com>
2021-02-01 00:46:00 +0300

    change to fun style

--- src/components/user/login/LoginForm.jsx
+++ src/components/user/login/LoginForm.jsx
@@ -1,101 +1,92 @@
-import React, {Component} from 'react'
-import {Button, Form, Input, notification} from 'antd'
+import React, { Component, useState } from 'react'
+import { Button, Form, Input, notification } from 'antd'
 import UserOutlined from '@ant-design/icons/lib/icons/UserOutlined'
 import LockOutlined from '@ant-design/icons/lib/icons/LockOutlined'
-import {localizedStrings} from '../../util/localization'
-import {Link} from 'react-router-dom'
+import { localizedStrings } from '../../util/localization'
+import { Link } from 'react-router-dom'
 import s from './Login.module.css'
-import {ACCESS_TOKEN, REFRESH_TOKEN} from '../../../constants'
-import {loginRequest} from '../../util/utilsAPI'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../../../constants'
+import { loginRequest } from '../../util/utilsAPI'
 import SocialLogin from './SocialLogin'
-import "./Login.module.css"
+import './Login.module.css'
 
-class LoginForm extends Component {
-    state = {
-        email: '',
-        password: ''
+const LoginForm = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () => {
+    const emailRequest = {
+      email: email,
+      password: password
     }
+    loginRequest(emailRequest)
+      .then(response => {
+        localStorage.setItem(ACCESS_TOKEN, response.accessToken)
+        localStorage.setItem(REFRESH_TOKEN, response.refreshToken)
 
-    handleSubmit = () => {
-        const emailRequest = {
-            email: this.state.email,
-            password: this.state.password
-        }
-        loginRequest(emailRequest)
-            .then(response => {
-                localStorage.setItem(ACCESS_TOKEN, response.accessToken)
-                localStorage.setItem(REFRESH_TOKEN, response.refreshToken)
+        props.onLogin()
+      }).catch(() => {
+      notification.error({
+        message: localizedStrings.alertAppName,
+        description: localizedStrings.alertWrongEmailOrPassword
+      })
+    })
+  }
 
-                this.props.onLogin()
-            }).catch(() => {
-            notification.error({
-                message: localizedStrings.alertAppName,
-                description: localizedStrings.alertWrongEmailOrPassword
-            })
-        })
-    }
+  const handleLoginChange = (event) => {
+    const value = event.target.value
+    setEmail(value)
+  }
 
-    handleLoginChange = (event) => {
-        const value = event.target.value
-        this.setState({
-            email: value
-        })
-    }
+  const handlePasswordChange = (event) => {
+    const value = event.target.value
+    setPassword(value)
+  }
 
-    handlePasswordChange = (event) => {
-        const value = event.target.value
-        this.setState({
-            password: value
-        })
-    }
+    return (
+      <Form
+        className={s.form}
+        onFinish={handleSubmit}>
+        <Form.Item
+          name='email'
+          rules={[{ required: true, message: localizedStrings.alertBadEmail }]}
+          onChange={handleLoginChange}>
 
-    render() {
-        return (
-            <Form
-                className={s.form}
-                onFinish={this.handleSubmit}>
-                <Form.Item
-                    name="email"
-                    rules={[{required: true, message: localizedStrings.alertBadEmail}]}
-                    onChange={this.handleLoginChange}>
+          <Input prefix={<UserOutlined />}
+                 size='large'
+                 type={'email'}
+                 name='email'
+                 placeholder={localizedStrings.email} />
+        </Form.Item>
 
-                    <Input prefix={<UserOutlined/>}
-                           size="large"
-                           type={"email"}
-                           name="email"
-                           placeholder={localizedStrings.email}/>
-                </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[{ required: true, message: localizedStrings.alertBadPassword }]}
+          onChange={handlePasswordChange}>
 
-                <Form.Item
-                    name="password"
-                    rules={[{required: true, message: localizedStrings.alertBadPassword}]}
-                    onChange={this.handlePasswordChange}>
+          <Input.Password
+            prefix={<LockOutlined />}
+            autoComplete={'current-password'}
+            size='large'
+            name='password'
+            type='password'
+            placeholder={localizedStrings.password} />
+        </Form.Item>
 
-                    <Input.Password
-                        prefix={<LockOutlined/>}
-                        autoComplete={"current-password"}
-                        size="large"
-                        name="password"
-                        type="password"
-                        placeholder={localizedStrings.password}/>
-                </Form.Item>
+        <SocialLogin />
 
-                <SocialLogin/>
-
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" size="large" className="login-form-button">
-                        {localizedStrings.login}
-                    </Button>
-                    {localizedStrings.or}
-                    <Link to="/sign-up">
-                        {localizedStrings.loginFormRegisterNow}
-                    </Link>
-                </Form.Item>
-
-            </Form>
-
-        )
-    }
+        <Form.Item>
+          <Button type='primary' htmlType='submit'
+                  size='large' className='login-form-button'>
+            {localizedStrings.login}
+          </Button>
+          {localizedStrings.or}
+          <Link to='/sign-up'>
+            {localizedStrings.loginFormRegisterNow}
+          </Link>
+        </Form.Item>
+      </Form>
+    )
 }
 
 export default LoginForm
